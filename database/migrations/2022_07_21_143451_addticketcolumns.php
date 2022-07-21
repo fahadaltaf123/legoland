9d1d3069c3efc3efd9ab9353064278384fb472d1
m.faizanaltaf <m.faizanaltaf@gmail.com>
2022-07-21 19:42:17 +0500

    cols

---
+++ database/migrations/2022_07_21_143451_addticketcolumns.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Addticketcolumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->float('deposit_amt')->nullable(true);
+            $table->string('deposit_type')->nullable(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
